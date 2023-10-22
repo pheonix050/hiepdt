7933458a661ac09fa8e09f0c14db9f9d7e520c79
Hiep Nguyen <hiepnguyendt97@gmail.com>
2023-10-22 21:47:30 +0700

    admin
    commit

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,31 +1,45 @@
-const pg = require('pg');
+//const pg = require('pg');
 
 // Connection Pool
-let connection = pg.createConnection({
-  host: process.env.DB_HOST,
+
+
+const { Client } = require('pg')
+const client = new Client({
   user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  host:  process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password:  process.env.DB_PASS,
+  port: 5432,
+})
+client.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected pg!");
+
 });
 
 // View Users
-exports.view = (req, res) => {
+exports.view = async (req, res) => {
   // User the connection
-  connection.query('SELECT * FROM user WHERE status = "active"', (err, rows) => {
-    // When done with the connection, release it
-    if (!err) {
-      let removedUser = req.query.removed;
-      res.render('home', { rows, removedUser });
-    } else {
-      console.log(err);
-    }
-    console.log('The data from user table: \n', rows);
-  });
+  const qr = await client.query('SELECT * FROM fcj_user ORDER BY id ASC')
+  const kq = qr.rows;
+  //console.log(kq)
+   let removedUser = req.query.removed;
+    res.render('home', { kq,removedUser });
+  // connection.query('SELECT * FROM user WHERE status = "active"', (err, rows) => {
+  //   // When done with the connection, release it
+  //   if (!err) {
+  //     let removedUser = req.query.removed;
+  //     res.render('home', { rows, removedUser });
+  //   } else {
+  //     console.log(err);
+  //   }
+  //   console.log('The data from user table: \n', rows);
+  // });
 }
 
 // Find User by Search
-exports.find = (req, res) => {
-  let searchTerm = req.body.search;
+exports.find = async(req, res) => {
+  /* let searchTerm = req.body.search;
   // User the connection
   connection.query('SELECT * FROM user WHERE first_name LIKE ? OR last_name LIKE ?', ['%' + searchTerm + '%', '%' + searchTerm + '%'], (err, rows) => {
     if (!err) {
@@ -34,7 +48,16 @@ exports.find = (req, res) => {
       console.log(err);
     }
     console.log('The data from user table: \n', rows);
-  });
+  }); */
+  
+   const searchTerm = req.body.search;
+   console.log(searchTerm);
+   const qr = await client.query('SELECT * FROM fcj_user WHERE first_name LIKE ? ',['%' + searchTerm + '%']);
+   const kq = qr.rows;
+   console.log(kq);
+   let removedUser = req.query.removed;
+   res.render('home', { kq,removedUser });
+  
 }
 
 exports.form = (req, res) => {
@@ -43,39 +66,49 @@ exports.form = (req, res) => {
 
 // Add new user
 exports.create = (req, res) => {
-  const { first_name, last_name, email, phone, comments } = req.body;
-  let searchTerm = req.body.search;
-
-  // User the connection
-  connection.query('INSERT INTO user SET first_name = ?, last_name = ?, email = ?, phone = ?, comments = ?', [first_name, last_name, email, phone, comments], (err, rows) => {
-    if (!err) {
-      res.render('add-user', { alert: 'User added successfully.' });
-    } else {
-      console.log(err);
-    }
-    console.log('The data from user table: \n', rows);
-  });
+  const { first_name, last_name, email, phone, comments} = req.body 
+  const qr = client.query('INSERT INTO fcj_user (first_name, last_name, email, phone, comments) VALUES ($1, $2, $3, $4, $5)', [first_name, last_name, email, phone, comments]);
+  const kq = qr.rows;
+  console.log(kq);
+  let removedUser = req.query.removed;
+    res.render('add-user', { kq, alert: `${first_name} has been created.` });
 }
 
 
 // Edit user
 exports.edit = (req, res) => {
   // User the connection
-  connection.query('SELECT * FROM user WHERE id = ?', [req.params.id], (err, rows) => {
+  /* connection.query('SELECT * FROM user WHERE id = ?', [req.params.id], (err, rows) => {
     if (!err) {
       res.render('edit-user', { rows });
     } else {
       console.log(err);
     }
     console.log('The data from user table: \n', rows);
-  });
-}
+  }); */
+  const { first_name, last_name, email, phone,comments } = req.body
+  const {userid} = req.params.id
+  console.log(userid)
+    const qr= client.query('UPDATE fcj_user SET first_name = $1, last_name= $2, email= $3, phone= $4,comments= $5 WHERE id = $6',[first_name, last_name, email, phone,comments , userid])
+    const kq= qr.rows;
+    console.log(kq);
+    let removedUser = req.query.removed;
+    res.render('edit-user', { kq, alert: `${first_name} has been updated.` });
+
+      }
 
 
 // Update User
 exports.update = (req, res) => {
-  const { first_name, last_name, email, phone, comments } = req.body;
-  // User the connection
+  const { first_name, last_name, email, phone,comments } = req.body
+  const {userid} = req.params
+  console.log(userid)
+    const qr= client.query('UPDATE fcj_user SET first_name = $1, last_name= $2, email= $3, phone= $4,comments= $5 WHERE id = $6',[first_name, last_name, email, phone,comments , userid])
+    const kq= qr.rows;
+    console.log(kq);
+    let removedUser = req.query.removed;
+    res.render('edit-user', { kq, alert: `${first_name} has been updated.` });
+  /* // User the connection
   connection.query('UPDATE user SET first_name = ?, last_name = ?, email = ?, phone = ?, comments = ? WHERE id = ?', [first_name, last_name, email, phone, comments, req.params.id], (err, rows) => {
 
     if (!err) {
@@ -94,7 +127,7 @@ exports.update = (req, res) => {
       console.log(err);
     }
     console.log('The data from user table: \n', rows);
-  });
+  }); */
 }
 
 // Delete User
@@ -116,7 +149,7 @@ exports.delete = (req, res) => {
 
   // Hide a record
 
-  connection.query('UPDATE user SET status = ? WHERE id = ?', ['removed', req.params.id], (err, rows) => {
+  /* connection.query('UPDATE user SET status = ? WHERE id = ?', ['removed', req.params.id], (err, rows) => {
     if (!err) {
       let removedUser = encodeURIComponent('User successeflly removed.');
       res.redirect('/?removed=' + removedUser);
@@ -124,7 +157,11 @@ exports.delete = (req, res) => {
       console.log(err);
     }
     console.log('The data from beer table are: \n', rows);
-  });
+  }); */
+  const userid = parseInt(req.params.id);
+  const qr= client.query('DELETE FROM fcj_user WHERE id = $1', [userid])
+  const kq =qr.rows;
+  console.log(kq);
 
 }
 
@@ -132,13 +169,8 @@ exports.delete = (req, res) => {
 exports.viewall = (req, res) => {
 
   // User the connection
-  connection.query('SELECT * FROM user WHERE id = ?', [req.params.id], (err, rows) => {
-    if (!err) {
-      res.render('view-user', { rows });
-    } else {
-      console.log(err);
-    }
-    console.log('The data from user table: \n', rows);
-  });
+  const qr= client.query('SELECT * FROM fcj_user WHERE id = $1', [req.params.id])
+  const kq = qr.rows;
+  console.log(kq)
 
 }
